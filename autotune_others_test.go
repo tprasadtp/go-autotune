806486603e64a5f4fb23f613d0b5f644f5bec7c8
Prasad Tengse <10941447+tprasadtp@users.noreply.github.com>
2023-12-21 18:14:24 +0100

    fix: make importing package autotune no-op on unsupported platforms (#10)

--- autotune_others_test.go
+++ autotune_others_test.go
@@ -16,15 +16,11 @@ import (
 )
 
 func TestOthers(t *testing.T) {
-	if os.Getenv("GOMAXPROCS") != "" {
-		t.Skipf("GOMAXPROCS env variable is set")
-	}
-
-	if os.Getenv("GOMEMLIMIT") != "" {
-		t.Skipf("GOMEMLIMIT env variable is set")
-	}
-
 	t.Run("GOMAXPROCS", func(t *testing.T) {
+		if os.Getenv("GOMAXPROCS") != "" {
+			t.Skipf("GOMAXPROCS env variable is set")
+		}
+
 		v := maxprocs.Current()
 		if v != runtime.NumCPU() {
 			t.Errorf("expected=%d, got=%d", runtime.NumCPU(), v)
@@ -32,6 +28,9 @@ func TestOthers(t *testing.T) {
 	})
 
 	t.Run("GOMEMLIMIT", func(t *testing.T) {
+		if os.Getenv("GOMEMLIMIT") != "" {
+			t.Skipf("GOMEMLIMIT env variable is set")
+		}
 		v := memlimit.Current()
 		if v != math.MaxInt64 {
 			t.Errorf("expected=%d, got=%d", math.MinInt64, v)
