5f0df57c490021e825be5d4303e08ebe383c9d33
Prasad Tengse <tprasadtp@users.noreply.github.com>
2023-12-21 04:32:41 +0100

    test: add macOS tests for autotune package

---
+++ autotune_others_test.go
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: Copyright 2023 Prasad Tengse
+// SPDX-License-Identifier: MIT
+
+//go:build !linux && !windows
+
+package autotune_test
+
+import (
+	"math"
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/tprasadtp/go-autotune/maxprocs"
+	"github.com/tprasadtp/go-autotune/memlimit"
+)
+
+func TestOthers(t *testing.T) {
+	if os.Getenv("GOMAXPROCS") != "" {
+		t.Skipf("GOMAXPROCS env variable is set")
+	}
+
+	if os.Getenv("GOMEMLIMIT") != "" {
+		t.Skipf("GOMEMLIMIT env variable is set")
+	}
+
+	t.Run("GOMAXPROCS", func(t *testing.T) {
+		v := maxprocs.Current()
+		if v != runtime.NumCPU() {
+			t.Errorf("expected=%d, got=%d", runtime.NumCPU(), v)
+		}
+	})
+
+	t.Run("GOMEMLIMIT", func(t *testing.T) {
+		v := memlimit.Current()
+		if v != math.MaxInt64 {
+			t.Errorf("expected=%d, got=%d", math.MinInt64, v)
+		}
+	})
+}
